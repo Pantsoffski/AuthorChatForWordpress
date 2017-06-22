f92e4fd95ed6c4af79cbb5cc52ee0529ee125f4d
Piotr Pesta <piotr.pesta@gmail.com>
2017-06-22 16:20:19 +0200

    Initial commit

---
+++ app/src/main/java/pl/ordin/authorchatforwordpress/HttpGetRequest.java
@@ -0,0 +1,74 @@
+package pl.ordin.authorchatforwordpress;
+
+import android.os.AsyncTask;
+import android.util.JsonReader;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+/**
+ * Class {@link HttpGetRequest} is responsible for url (wiki) leech.
+ */
+class HttpGetRequest extends AsyncTask<String, Void, String> {
+    private static final String REQUEST_METHOD = "GET";
+    private static final int READ_TIMEOUT = 15000;
+    private static final int CONNECTION_TIMEOUT = 15000;
+
+    @Override
+    protected String doInBackground(String... params) {
+        String stringUrl = params[0];
+        String result = "";
+
+        try {
+            //Create a URL object holding our url
+            URL myUrl = new URL(stringUrl);
+
+            //Create a connection
+            HttpURLConnection connection = (HttpURLConnection)
+                    myUrl.openConnection();
+
+            //Set methods and timeouts
+            connection.setRequestMethod(REQUEST_METHOD);
+            connection.setReadTimeout(READ_TIMEOUT);
+            connection.setConnectTimeout(CONNECTION_TIMEOUT);
+
+            //Connect to our url
+            connection.connect();
+
+            //Create a new InputStreamReader
+            InputStreamReader streamReader = new
+                    InputStreamReader(connection.getInputStream());
+
+            JsonReader jsonReader = new JsonReader(streamReader);
+
+            String value;
+            jsonReader.beginObject(); // Start processing the JSON object
+            while (jsonReader.hasNext()) { // Loop through all keys
+                String key = jsonReader.nextName(); // Fetch the next key
+                if (key.equals("current_user_url")) { // Check if desired key
+                    // Fetch the value as a String
+                    value = jsonReader.nextString();
+
+                    result = value;
+
+                    break; // Break out of the loop
+                } else {
+                    jsonReader.skipValue(); // Skip values of other keys
+                }
+            }
+
+            jsonReader.close();
+
+            connection.disconnect();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            result = null;
+        }
+
+        return result;
+    }
+
+}
---
+++ app/src/main/java/pl/ordin/authorchatforwordpress/MainActivity.java
@@ -0,0 +1,33 @@
+package pl.ordin.authorchatforwordpress;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        TextView justText = (TextView) findViewById(R.id.justText);
+
+        //Instantiate new instance of our class
+        HttpGetRequest getRequest = new HttpGetRequest();
+
+        String result;
+        //Perform the doInBackground method, passing in our url
+        try {
+            result = getRequest.execute("https://api.github.com/").get();
+            if (result == null) {
+                result = "";
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            result = "";
+        }
+
+        justText.setText(result);
+    }
+}
